757b14b8bc7dd82d3df888c55608ad5c3262da02
tbourke <devnull@localhost>
2008-04-01 10:58:28 +0000

    Fixed a bug when handling unicode in csv files,   reported by Patrick Kenny <ptmkenny@gmail.com>
    Added the --gui-csv-edit feature.

--- svgtoquiz.py
+++ svgtoquiz.py
@@ -38,12 +38,19 @@ Mnemosyne."""
 #----------------------------------------------------------------------
 
 import sys, re, os, csv, random, codecs
+from tempfile import mkdtemp
 import xml.dom.minidom
 from optparse import OptionParser
+
+# for the gui csv editor
+from Tkinter import *
+import tkMessageBox
+from PIL import Image, ImageTk
 
 #----------------------------------------------------------------------
 
 INKSCAPE_DPI = 90.0
+IGNORE = '_ignore_'
 
 class Options:
     parser = OptionParser(usage="%prog [options] <name>",
@@ -51,7 +58,7 @@ class Options:
 			  description=DESCRIPTION)
     parser.add_option('-c', '--category', dest='category', metavar='CATEGORY',
 		      help='specify the category')
-    parser.add_option('-z', '--zoom', dest='zoom', metavar='FLOAT',
+    parser.add_option('-z', '--zoom', dest='zoom', metavar='FLOAT', default=1.0,
 		      help='enlarge or reduce the produced images')
     parser.add_option('-s', '--id-regex', dest='id_regex', metavar='REGEX',
 		      help='match path ids (regex with one bracketed group)')
@@ -82,6 +89,9 @@ class Options:
     parser.add_option('--no-prefix', action='store_false',
 		      dest='prefix_names', default=True,
 		      help='do not add a prefix to filenames')
+    parser.add_option('-e', '--gui-csv-edit', action='store_true',
+		      dest='run_csvgui', default=False,
+		      help='run the gui csv editor')
 
     def setName(self, name):
 	re.sub(r'.svg$', '', name)
@@ -132,6 +142,7 @@ class Options:
 	self.create_inverse = options.create_inverse
 	self.color	    = options.color
 	self.match_csv	    = options.match_csv
+	self.run_csvgui	    = options.run_csvgui
 
 	if options.prefix_names: self.prefix = self.name + '_'
 	else:			 self.prefix = ''
@@ -224,7 +235,7 @@ def svg_to_png(svg_path, png_path):
 				       '--y-zoom', zoom,
 				       svg_path, png_path]))
 
-def make_state_maps(svg, name_map=None):
+def make_state_maps(dir_path, svg, prefix='', name_map=None):
     """
     Given an svg map, produce a separate svg map for each state.
     If name_map is given and options.match_csv=True, then names
@@ -241,8 +252,8 @@ def make_state_maps(svg, name_map=None):
 	if options.show_names: print name
 	prev_fill = fill_style(node, options.color)
 
-	svg_path = os.path.join(options.dstpath, options.prefix + name + '.svg')
-	png_path = os.path.join(options.dstpath, options.prefix + name + '.png')
+	svg_path = os.path.join(dir_path, prefix + name + '.svg')
+	png_path = os.path.join(dir_path, prefix + name + '.png')
 
 	fp = codecs.open(svg_path, 'w', 'utf-8')
 	svg.writexml(fp)
@@ -265,6 +276,18 @@ def read_name_map(csv_path):
     for row in reader:
 	data[row[0]] = row[1]
     return data
+
+def non_ignored(name_map):
+    for (key, val) in name_map.iteritems():
+	if val != IGNORE: yield [key, val]
+    return
+
+def write_name_map(csv_path, name_map):
+    """
+    Write non-IGNORE elements of the name_map hash into a csv file.
+    """
+    writer = csv.writer(open(csv_path, 'wb'))
+    writer.writerows(non_ignored(name_map))
 
 #----------------------------------------------------------------------
 def makeTextNode(dom, tagName, text):
@@ -414,8 +437,11 @@ def make_questions(names, name_map=None, cat='Map', qimgfile=None):
 	else: fullname = n.replace('_', ' ')
 
 	n_path = os.path.join(options.exportpath, options.prefix + n + '.png')
-	q = '<b>%s?</b>\n%s' % (fullname, qimg)
-	a = '<b>%s</b>\n<img src="%s">' % (fullname, n_path)
+	q = '<b>%s?</b>\n%s' % (fullname,
+				qimg.encode('utf-8', 'xmlcharrefreplace'))
+	a = '<b>%s</b>\n<img src="%s">' % (fullname,
+					   n_path.encode('utf-8',
+							 'xmlcharrefreplace'))
 	if options.create_inverse:
 	    qinv = '<img src="%s">' % n_path
 	    ainv = '<b>' + fullname + '</b>'
@@ -423,6 +449,166 @@ def make_questions(names, name_map=None, cat='Map', qimgfile=None):
 	e.addItem(q, a, cat, qinv, ainv)
 
     return e
+
+#----------------------------------------------------------------------
+
+class Application(Frame):
+    def showImage(self):
+	path = os.path.join(self.tmpdir, self.names[self.current] + '.png')
+	self.image = ImageTk.PhotoImage(Image.open(path))
+	self.labImage['image'] = self.image
+    
+    def showStatus(self):
+	self.labStatus['text'] = '%d / %d' % (self.current+1, self.numnames)
+    
+    def showCurrentName(self):
+	if self.name_map.has_key(self.names[self.current]):
+	    self.dataName.set(self.name_map[self.names[self.current]])
+	else:
+	    self.dataName.set(self.names[self.current])
+    
+    def updateWindow(self):
+	self.showImage()
+	self.showCurrentName()
+	self.showStatus()
+    
+    def setCurrentName(self, value=None):
+	if value == None:
+	    value = self.dataName.get()
+
+	if (not self.name_map.has_key(self.names[self.current])
+	    or self.name_map[self.names[self.current]] != value):
+	    self.name_map[self.names[self.current]] = value
+	    self.dirty = True
+    
+    def skip(self, i):
+	return (self.dataHideIgnored.get()
+		and self.name_map.has_key(self.names[i])
+		and self.name_map[self.names[i]] == IGNORE)
+
+    def gotoNext(self):
+	curr = self.current + 1
+	while (curr < self.numnames and self.skip(curr)):
+	    curr += 1
+
+	if (curr < self.numnames):
+	    self.setCurrentName()
+	    self.current = curr
+	    self.updateWindow()
+
+    def gotoPrev(self):
+	curr = self.current - 1
+	while (curr >= 0 and self.skip(curr)):
+	    curr -= 1
+
+	if (curr >= 0):
+	    self.setCurrentName()
+	    self.current = curr
+	    self.updateWindow()
+
+    def ignore(self):
+	self.dataName.set(IGNORE)
+	self.gotoNext()
+
+    def saveData(self):
+	self.setCurrentName()
+	if self.dirty and tkMessageBox.askokcancel(message='Really save?'):
+	    write_name_map(options.srcpath_csv, self.name_map)
+	    self.dirty = False
+    
+    def cleanTmpDir(self):
+	for f in os.listdir(self.tmpdir):
+	    os.unlink(os.path.join(self.tmpdir, f))
+	os.rmdir(self.tmpdir)
+
+    def quitApp(self):
+	self.setCurrentName()
+	if (not self.dirty) or tkMessageBox.askokcancel(
+		message='The data have not been saved. Really quit?'):
+	    self.cleanTmpDir()
+	    self.quit()
+
+    def createWidgets(self):
+	self.frameGraphics = Frame(self)
+	self.frameData     = Frame(self)
+	self.frameButtons  = Frame(self)
+	self.frameGraphics.pack(side='top',    fill='both', expand='yes')
+	self.frameButtons.pack (side='bottom', fill='x',    expand='no')
+	self.frameData.pack    (side='bottom', fill='x',    expand='no')
+
+	self.labImage = Label(self.frameGraphics)
+	self.labImage.pack(fill='both', expand='yes')
+
+	self.dataHideIgnored = IntVar()
+	self.chkHideIgnored = Checkbutton(self.frameButtons,
+					  text="skip ignored",
+					  variable=self.dataHideIgnored)
+	self.chkHideIgnored.pack(side='left')
+	self.dataHideIgnored.set(1)
+
+        self.butQuit = Button(self.frameButtons, text='quit',
+			      command=self.quitApp)
+        self.butQuit.pack(side='right')
+
+        self.butSave = Button(self.frameButtons, text='save',
+			      command=self.saveData)
+        self.butSave.pack(side='right')
+
+	self.labStatus = Label(self.frameButtons, text='status')
+	self.labStatus.pack(side='right', fill='x', expand='yes')
+
+        self.butPrev = Button(self.frameData, text='prev',
+			      command=self.gotoPrev)
+        self.butPrev.pack(side='left')
+
+        self.butIgnore = Button(self.frameData, text='ignore',
+				command=self.ignore)
+        self.butIgnore.pack(side='right')
+
+        self.butNext = Button(self.frameData, text='next',
+			      command=self.gotoNext)
+        self.butNext.pack(side='right')
+
+	self.dataName = StringVar()
+	self.dataName.set("id field")
+
+	self.entryName = Entry(self.frameData, textvariable=self.dataName)
+	self.entryName.pack(side='left', fill='x', expand='yes')
+
+    def __init__(self, svg, name_map={}, master=None):
+	try: mapdom = xml.dom.minidom.parse(options.srcpath_svg)
+	except IOError, e:
+	    print >> sys.stderr, 'Could not open ' + options.srcpath_svg,
+	    print >> sys.stderr, '(' + e.strerror + ')'
+	    sys.exit(1)
+	self.tmpdir = mkdtemp()
+
+	if name_map:
+	    self.name_map = name_map
+	else:
+	    self.name_map = {}
+
+	print 'Generating images (' + self.tmpdir + ')...'
+	self.names = make_state_maps(self.tmpdir, svg, '', name_map)
+	self.numnames = len(self.names)
+	if self.numnames == 0:
+	    print >> sys.stderr, 'No paths in file.'
+	    sys.exit(1)
+	self.current = 0
+	mapdom.unlink()
+
+        Frame.__init__(self, master)
+        self.pack(fill='both', expand='yes')
+        self.createWidgets()
+	self.dirty = False
+
+	self.updateWindow()
+
+def guicsv_main(svg, name_map):
+    root = Tk()
+    app = Application(master=root, svg=svg, name_map=name_map)
+    app.mainloop()
+    root.destroy()
 
 #----------------------------------------------------------------------
 
@@ -447,6 +633,10 @@ def main():
     else:
 	name_map = None
 
+    if options.run_csvgui:
+	guicsv_main(svg, name_map)
+	return
+
     try:
 	try:	os.stat(options.dstpath)
 	except: os.makedirs(options.dstpath)
@@ -455,7 +645,7 @@ def main():
 	print >> sys.stderr, '(' + e.strerror + ')'
 	sys.exit(1)
 
-    names = make_state_maps(svg, name_map)
+    names = make_state_maps(options.dstpath, svg, options.prefix, name_map)
     mapdom.unlink()
 
     export = make_questions(names, name_map, options.category, options.q_img)
@@ -469,4 +659,3 @@ def main():
 
 options.parseArguments(sys.argv[1:])
 main()
-
