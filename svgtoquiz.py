8d328709ea7d3c5889de5298580c7716d2465853
tbourke <devnull@localhost>
2008-04-02 06:24:02 +0000

    Split the script into two halves (to isolate the gui dependencies) At the --only-vice-versa option at the suggestion of Patrick Kenny

--- svgtoquiz.py
+++ svgtoquiz.py
@@ -20,13 +20,17 @@
 #   * Developed in python 2.5
 #   * Requires rsvg (part of librsvg2)
 #     although it could be modified to support any svg->png converter.
+#   * threading
+#   * xml.dom.minidom
 #
 RSVG_PATH = '/usr/bin/rsvg'
+RSVG_PATH = '/usr/local/bin/rsvg'
 INKSCAPE_PATH = '/usr/bin/inkscape'
+INKSCAPE_PATH = '/usr/local/bin/inkscape'
 SVGTOPNG = 'rsvg'
 # SVGTOPNG = 'inkscape'
 
-VERSION = '1.2.0'
+VERSION = '1.2.1'
 DESCRIPTION ="""Work through a given svg file, turning every path whose
 id matches a given regular expression into a Mnemosyne entry where the
 question is the id, or looked up in a separate csv file, and the answer is
@@ -37,16 +41,11 @@ Mnemosyne."""
 
 #----------------------------------------------------------------------
 
-import sys, re, os, csv, random, codecs, time
-import cStringIO
+import sys, re, os, random, codecs
+import csv, cStringIO
 from tempfile import mkdtemp
 import xml.dom.minidom
 from optparse import OptionParser
-
-# for the gui csv editor
-from Tkinter import *
-import tkMessageBox
-from PIL import Image, ImageTk
 import threading
 
 #----------------------------------------------------------------------
@@ -88,6 +87,9 @@ class Options:
     parser.add_option('--no-vice-versa', action='store_false',
 		      dest='create_inverse', default=True,
 		      help='do not produce vice-versa entries')
+    parser.add_option('--only-vice-versa', action='store_false',
+		      dest='create_normal', default=True,
+		      help='only produce vice-versa entries')
     parser.add_option('--no-prefix', action='store_false',
 		      dest='prefix_names', default=True,
 		      help='do not add a prefix to filenames')
@@ -141,7 +143,9 @@ class Options:
 	self.zoom	    = options.zoom
 	self.random_order   = options.random_order
 	self.show_names     = options.show_names
-	self.create_inverse = options.create_inverse
+	self.create_normal  = options.create_normal
+	self.create_inverse = (options.create_inverse or
+			       not options.create_normal)
 	self.color	    = options.color
 	self.match_csv	    = options.match_csv
 	self.run_csvgui	    = options.run_csvgui
@@ -353,10 +357,12 @@ def non_ignored(name_map):
 	if val != IGNORE: yield [key, val]
     return
 
-def write_name_map(csv_path, name_map):
+def write_name_map(name_map, csv_path=None):
     """
     Write non-IGNORE elements of the name_map hash into a csv file.
     """
+    if csv_path == None: csv_path = options.srcpath_csv
+
     writer = UnicodeWriter(open(csv_path, 'wb'))
     writer.writerows(non_ignored(name_map))
 
@@ -517,7 +523,11 @@ def make_questions(names, name_map=None, cat='Map', qimgfile=None):
 	    qinv = '<img src="%s">' % n_path
 	    ainv = '<b>' + fullname + '</b>'
 	else: (qinv, ainv) = (None, None)
-	e.addItem(q, a, cat, qinv, ainv)
+
+	if options.create_normal:
+	    e.addItem(q, a, cat, qinv, ainv)
+	else:
+	    e.addItem(qinv, ainv, cat)
 
     return e
 
@@ -560,6 +570,12 @@ class ConverterThread (threading.Thread):
 	self.lock.release()
 	return r
 
+    def getNames(self):
+	self.lock.acquire()
+	r = [unicode(name) for (name, node) in self.namesAndNodes]
+	self.lock.release()
+	return r
+
     def numberDone(self):
 	self.lock.acquire()
 	r = self.numDone
@@ -591,195 +607,6 @@ class ConverterThread (threading.Thread):
 
 #----------------------------------------------------------------------
 
-class Application(Frame):
-    def showImage(self):
-	if self.converter.isDone(self.current):
-	    path = os.path.join(self.tmpdir, self.names[self.current] + '.png')
-	    img = Image.open(path)
-	    self.lastImageSize = img.size
-	    self.image = ImageTk.PhotoImage(img)
-	    self.labImage['image'] = self.image
-	else:
-	    if self.blankImage == None:
-		self.blankImage = ImageTk.PhotoImage(Image.new('RGB',
-							       self.lastImageSize,
-							       'white'))
-	    self.labImage['image'] = self.blankImage
-	    self.converter.setCurrent(self.current)
-	    self.after(500, self.showImage)
-    
-    def showStatus(self):
-	numdone = self.converter.numberDone()
-	self.labStatus['text'] = '%d / %d [%d done]' % (self.current+1,
-						        self.numnames,
-						        numdone)
-	if numdone < self.numnames:
-	    self.after(100, self.showStatus)
-    
-    def showCurrentName(self):
-	if self.name_map.has_key(self.names[self.current]):
-	    self.dataName.set(self.name_map[self.names[self.current]])
-	else:
-	    self.dataName.set(self.names[self.current])
-    
-    def updateWindow(self):
-	self.showImage()
-	self.showCurrentName()
-	self.showStatus()
-    
-    def setCurrentName(self, value=None):
-	if value == None:
-	    value = unicode(self.dataName.get())
-
-	if (not self.name_map.has_key(self.names[self.current])
-	    or self.name_map[self.names[self.current]] != value):
-	    self.name_map[self.names[self.current]] = value
-	    self.dirty = True
-    
-    def skip(self, i):
-	return (self.dataHideIgnored.get()
-		and self.name_map.has_key(self.names[i])
-		and self.name_map[self.names[i]] == IGNORE)
-
-    def gotoNext(self):
-	curr = self.current + 1
-	while (curr < self.numnames and self.skip(curr)):
-	    curr += 1
-
-	if (curr < self.numnames):
-	    self.setCurrentName()
-	    self.current = curr
-	    self.updateWindow()
-
-    def gotoPrev(self):
-	curr = self.current - 1
-	while (curr > 0 and self.skip(curr)):
-	    curr -= 1
-
-	if (curr >= 0 and not self.skip(curr)):
-	    self.setCurrentName()
-	    self.current = curr
-	    self.updateWindow()
-
-    def ignore(self):
-	self.dataName.set(IGNORE)
-	self.gotoNext()
-
-    def saveData(self):
-	self.setCurrentName()
-	if self.dirty and tkMessageBox.askokcancel(message='Really save?'):
-	    write_name_map(options.srcpath_csv, self.name_map)
-	    self.dirty = False
-    
-    def cleanTmpDir(self):
-	for f in os.listdir(self.tmpdir):
-	    os.unlink(os.path.join(self.tmpdir, f))
-	os.rmdir(self.tmpdir)
-
-    def quitApp(self):
-	self.setCurrentName()
-	if (not self.dirty) or tkMessageBox.askokcancel(
-		message='The data have not been saved. Really quit?'):
-	    self.converter.requestStop()
-	    self.converter.join(5)
-	    try: self.cleanTmpDir()
-	    except:
-		print >> sys.stderr, 'could not remove ' + self.tmpdir + '...'
-	    self.quit()
-
-    def createWidgets(self):
-	self.frameGraphics = Frame(self)
-	self.frameData     = Frame(self)
-	self.frameButtons  = Frame(self)
-	self.frameGraphics.pack(side='top',    fill='both', expand='yes')
-	self.frameButtons.pack (side='bottom', fill='x',    expand='no')
-	self.frameData.pack    (side='bottom', fill='x',    expand='no')
-
-	self.labImage = Label(self.frameGraphics)
-	self.labImage.pack(fill='both', expand='yes')
-
-	self.dataHideIgnored = IntVar()
-	self.chkHideIgnored = Checkbutton(self.frameButtons,
-					  text="skip ignored",
-					  variable=self.dataHideIgnored)
-	self.chkHideIgnored.pack(side='left')
-	self.dataHideIgnored.set(1)
-
-        self.butQuit = Button(self.frameButtons, text='quit',
-			      command=self.quitApp)
-        self.butQuit.pack(side='right')
-
-        self.butSave = Button(self.frameButtons, text='save',
-			      command=self.saveData)
-        self.butSave.pack(side='right')
-
-	self.labStatus = Label(self.frameButtons, text='status')
-	self.labStatus.pack(side='right', fill='x', expand='yes')
-
-        self.butPrev = Button(self.frameData, text='prev',
-			      command=self.gotoPrev)
-        self.butPrev.pack(side='left')
-
-        self.butIgnore = Button(self.frameData, text='ignore',
-				command=self.ignore)
-        self.butIgnore.pack(side='right')
-
-        self.butNext = Button(self.frameData, text='next',
-			      command=self.gotoNext)
-        self.butNext.pack(side='right')
-
-	self.dataName = StringVar()
-	self.dataName.set("id field")
-
-	self.entryName = Entry(self.frameData, textvariable=self.dataName)
-	self.entryName.pack(side='left', fill='x', expand='yes')
-
-    def __init__(self, dom, svg, name_map={}, master=None):
-	try: mapdom = xml.dom.minidom.parse(options.srcpath_svg)
-	except IOError, e:
-	    print >> sys.stderr, 'Could not open ' + options.srcpath_svg,
-	    print >> sys.stderr, '(' + e.strerror + ')'
-	    sys.exit(1)
-	self.tmpdir = mkdtemp()
-
-	if name_map:
-	    self.name_map = name_map
-	else:
-	    self.name_map = {}
-
-	print 'Generating images (' + self.tmpdir + ')...'
-
-	namesAndNodes = read_names_and_nodes(svg, name_map)
-	self.names = [unicode(name) for (name, node) in namesAndNodes]
-	self.numnames = len(self.names)
-	if self.numnames == 0:
-	    print >> sys.stderr, 'No paths in file.'
-	    sys.exit(1)
-	self.current = 0
-
-	self.converter = ConverterThread(dom,svg,namesAndNodes,self.tmpdir, '')
-	self.converter.start()
-	while not (self.converter.isDone(0)):
-	    time.sleep(.5)
-	self.lastImageSize = (0,0)
-	self.blankImage = None
-
-        Frame.__init__(self, master)
-        self.pack(fill='both', expand='yes')
-        self.createWidgets()
-	self.dirty = False
-
-	self.updateWindow()
-
-def guicsv_main(mapdom, svg, name_map):
-    root = Tk()
-    root.title('svgtoquiz: edit csv file')
-    app = Application(master=root, dom=mapdom, svg=svg, name_map=name_map)
-    app.mainloop()
-    root.destroy()
-
-#----------------------------------------------------------------------
-
 def main():
     try: mapdom = xml.dom.minidom.parse(options.srcpath_svg)
     except IOError, e:
@@ -802,8 +629,19 @@ def main():
 	name_map = None
 
     if options.run_csvgui:
-	guicsv_main(mapdom, svg, name_map)
-	return
+	if GUI:
+	    namesAndNodes = read_names_and_nodes(svg, name_map)
+	    tmpdir = mkdtemp()
+	    converter = ConverterThread(mapdom, svg, namesAndNodes, tmpdir, '')
+	    guicsv_main(name_map, converter, tmpdir, write_name_map)
+	else:
+	    print >> sys.stderr, 'Sorry, the GUI feature is not available.'
+	    print >> sys.stderr, 'One of the requirements is not satisfied:'
+	    print >> sys.stderr, '  * svgtoquiz_gui.py in same directory'
+	    print >> sys.stderr, '  * Tkinter and tkMessageBox (python-tk)'
+	    print >> sys.stderr, '  * Python Image Library (python-imaging,'
+	    print >> sys.stderr, '  *                       python-imaging-tk)'
+	    sys.exit(1)
 
     try:
 	try:	os.stat(options.dstpath)
@@ -827,5 +665,13 @@ def main():
     svg_to_png(options.srcpath_svg,
 	       os.path.join(options.dstpath, options.q_img))
 
+
+#sys.path.append(os.path.dirname(sys.argv[0]))
+try:
+    from svgtoquiz_gui import *
+    GUI=True
+except: GUI=False
+
 options.parseArguments(sys.argv[1:])
 main()
+
